64ca5bddbfa85c01fa528ca603c8a583769f4d63
Klhnikov Riton <parisot.eric.mail@gmail.com>
2018-08-26 15:28:19 +0200

    test

--- srcs/calc_app.py
+++ srcs/calc_app.py
@@ -45,6 +45,8 @@ class Calc(object):
                 print("Result : " + reduced)
         return (reduced, degree_val, result)
 
+
+
     def parse(self, equation, gui):
         if len(equation) > 0 and \
            re.match('^[a-zA-Z0-9+-/*^= ]+$', equation) and \
